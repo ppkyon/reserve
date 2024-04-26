c2cee5462d2764a4c72f54238c47d37197f00c44
ppkyon <ppkyon@gmail.com>
2024-04-26 16:59:30 +0900

    デバッグ

--- line/reserve/action/send.py
+++ line/reserve/action/send.py
@@ -585,6 +585,9 @@ def send(request):
                             )
 
     target_flow_tab = None
+    import logging
+    logger = logging.getLogger('development')
+    logger.info(request.POST.get('setting_id'))
     if ReserveOfflineSetting.objects.filter(display_id=request.POST.get('setting_id')).exists():
         setting = ReserveOfflineSetting.objects.filter(display_id=request.POST.get('setting_id')).first()
         for menu in ReserveOfflineFlowMenu.objects.filter(shop=shop, offline=setting).all():
