6d2b33891722b369223b821e49d49413662df753
rxgkxx <rxgkxx@gmail.com>
2024-04-29 01:31:02 +0900

    スライドメニュー部分のデザイン修正

--- static/js/side.js
+++ static/js/side.js
@@ -6,7 +6,7 @@ $(function () {
 
     if ($('.slide-user li').hasClass('active')) {
       $('.slide-user').addClass("slide-active");
-      $('.slide-user-area').addClass("active-cursor");
+      $('.slide-user-area').toggleClass("active-cursor");
     } else {
       $('.slide-user').removeClass("slide-active");
       $('.slide-user-area').removeClass("active-cursor");
@@ -14,7 +14,7 @@ $(function () {
 
     if ($('.slide-template li').hasClass('active')) {
       $('.slide-template').addClass("slide-active");
-      $('.slide-template-area').addClass("active-cursor");
+      $('.slide-template-area').toggleClass("active-cursor");
     } else {
       $('.slide-template').removeClass("slide-active");
       $('.slide-template-area').removeClass("active-cursor");
@@ -22,7 +22,7 @@ $(function () {
 
     if ($('.slide-setting li').hasClass('active')) {
       $('.slide-setting').addClass("slide-active");
-      $('.slide-setting-area').addClass("active-cursor");
+      $('.slide-setting-area').toggleClass("active-cursor");
     } else {
       $('.slide-setting').removeClass("slide-active");
       $('.slide-setting-area').removeClass("active-cursor");
