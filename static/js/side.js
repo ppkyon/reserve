0dbb6ea82e9a0c6264266056b842400e3bba74d0
ppkyon <ppkyon@gmail.com>
2024-04-29 01:59:22 +0900

    サイドメニュー更新

--- static/js/side.js
+++ static/js/side.js
@@ -3,30 +3,4 @@ $(function () {
       $(this).next().slideToggle(300);
       $(this).toggleClass("slide-open", 300);
     });
-
-    if ($('.slide-user li').hasClass('active')) {
-      $('.slide-user').addClass("slide-active");
-      $('.slide-user-area').toggleClass("active-cursor");
-    } else {
-      $('.slide-user').removeClass("slide-active");
-      $('.slide-user-area').removeClass("active-cursor");
-    };
-
-    if ($('.slide-template li').hasClass('active')) {
-      $('.slide-template').addClass("slide-active");
-      $('.slide-template-area').toggleClass("active-cursor");
-    } else {
-      $('.slide-template').removeClass("slide-active");
-      $('.slide-template-area').removeClass("active-cursor");
-    }
-
-    if ($('.slide-setting li').hasClass('active')) {
-      $('.slide-setting').addClass("slide-active");
-      $('.slide-setting-area').toggleClass("active-cursor");
-    } else {
-      $('.slide-setting').removeClass("slide-active");
-      $('.slide-setting-area').removeClass("active-cursor");
-    }
-
-
 });
