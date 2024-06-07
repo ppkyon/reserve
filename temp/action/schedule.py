4245df9460aa1263733524a5d7fd0aaef0dc85ee
ppkyon <ppkyon@gmail.com>
2024-06-07 22:31:37 +0900

    デバッグ

--- temp/action/schedule.py
+++ temp/action/schedule.py
@@ -867,12 +867,6 @@ def send(request):
                                                             if reception['setting']:
                                                                 if reception['setting'].id == offline_setting.id:
                                                                     if schedule_date == reception['from']:
-                                                                        if times.day == 14 and times.hour == 16:
-                                                                            import logging
-                                                                            logger = logging.getLogger('development')
-                                                                            logger.info(people_count)
-                                                                            logger.info(reception['facility'].count)
-                                                                            logger.info(same_count)
                                                                         if count_flg:
                                                                             if reception['facility'] and reception['facility'].count < people_count:
                                                                                 same_count = same_count - 1
@@ -952,6 +946,12 @@ def send(request):
                                                 reception_manager_list.append(manager_item)
                                     if manager_count <= 0 or facility_count <= 0:
                                         reception_flg = True
+                                        
+                                    if times.day == 14 and times.hour == 16:
+                                        import logging
+                                        logger = logging.getLogger('development')
+                                        logger.info(manager_count)
+                                        logger.info(facility_count)
                                     
                                     if reception_flg:
                                         if ReserveCalendarTime.objects.filter(calendar=reserve_calendar_date, time=schedule_time).exists():
